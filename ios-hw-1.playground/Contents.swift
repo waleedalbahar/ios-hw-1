302a962d894fa4d854bff456caa40840c0f7b1d3
waleedalbahar <waleedalbahar10@gmail.com>
2020-06-15 23:54:50 +0300

    homework

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -1,25 +1,22 @@
-/*
+                                       /*
  - - - -
  # السلام عليكم
  ## المتطلبات: اصلح الكودات البرمجية التالية
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
+var weightSum = myWeight; "+"; FriendWeight
 
 
-var weightSum = myWeight + FriendWeight
-
-
-
-var bmi = myWeight * (height * height)
+var bmi = myWeight *  (height * height)
 
 /*
 - - - -
@@ -38,35 +35,40 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Ahmad"
+var lastName1 = "AL Fares"
+var email1 = "ahmadalfares@gmail.com"
+var phoneNumber1 = "99632587"
+var age1 = 15
+var country1 = "Kuwait"
+var area1 = "Shuwaikh"
+var password1 = "ahmad3654"
+var isKuwaiti1 = true
 
 //Person 2
-
-
-
-
-
-
+var  firstName2 = "Salem"
+var lastName2 = "AL Subah"
+var email2 = "salsubah@gmail.com"
+var phonenumber2 = "60438797"
+var age2 = 16
+var country2 = "Bahrain"
+var area2 = "AL Seef"
+var Password2 = "s36598"
+var iskuwaiti2 = false
 
 
 
 
 //Person 3
-
-
-
-
-
-
-
+var FirstName3 = "Khaled"
+var LastName3 = " AL Khairafi"
+var email3 = "khaledalkhairafi43@gmail.com"
+var phonenumber3 = "99375421"
+var  age3 = 18
+var country3 = "Kuwait"
+var area3 = "Shamiya"
+var password3 = "K9M643"
+var iskuwaiti3 = true
 
 
 /*
@@ -75,10 +77,57 @@ var isKuwaiti = false
 * قم بمقارنة معلومات كل شخص مع الشخص الاخر
  * قارن الاعمار اذا كانت متساوية ام لا
  * قارن منطقة السكن اذا كانت متساوية ام لا
- * قارن اذا عمر الاشخاص اكبر من ١٨
+ * قارن اذا عمر الاشخاص اكبر من ١
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
- 
+
  */
+firstName1 != firstName2
+firstName1 != FirstName3
+ firstName2 !=  FirstName3
+                                       
+                                       
+                                       
+                                       
+lastName1 != lastName2
+lastName1 != LastName3
+lastName2 == LastName3
+
+                                       
+                                       
+email1 != email2
+email1 != email3
+email2 != email3
+
+                                       
+                                       
+ phoneNumber1 !=  phonenumber2
+  phoneNumber1 != phonenumber3
+  phonenumber2 == phonenumber3
+
+                                       
+                                       
+                                       
+age1 != age2
+age1 != age3
+age2 != age3
+age1 < age2
+age1  < age3
+age2 < age3
+
+                                       
+                                       
+                                       
+country1 != country2
+ country1 == country3
+country2 != country3
+
+                                       
+                                       
+                                       
+                                       
+area1 != area2
+area1 != area3
+area2 != area3
